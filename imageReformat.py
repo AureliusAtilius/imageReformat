94d297d137d2d620649434c124e26f987c7d5a5e
Josh <j.j.h.lindner@gmail.com>
2021-04-27 22:04:34 -0700

    Create file and outline.

---
+++ imageReformat.py
@@ -0,0 +1,14 @@
+#! bin/env/python3 
+import os, PIL
+from pathlib import Path
+
+#TODO: Move through each file in a folder
+
+#TODO: Open Image
+
+#TODO: Rotate image 
+
+#TODO: Resize image 
+
+#TODO: Save image as .jpg
+
